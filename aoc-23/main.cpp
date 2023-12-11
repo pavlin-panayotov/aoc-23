e222ee65c1800435386668568ccb42bcbf4590b8
pavlin.panayotov <panayotov1995@gmail.com>
2023-12-11 23:55:05 +0200

    Xcode project.

---
+++ aoc-23/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  aoc-23
+//
+//  Created by Pavlin Panayotov on 11.12.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
